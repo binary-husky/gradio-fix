37fdac3b1a6413d271ab0107f17f2cee26772346
binary-husky <qingxu.fu@outlook.com>
2023-12-15 17:01:28 +0800

    update build bash

--- build_script.sh
+++ build_script.sh
@@ -1,8 +1,10 @@
 # change version.txt and run `bash build_script.sh`
 rm -r dist
+nvm use 18
 bash scripts/build_frontend.sh && bash scripts/install_gradio.sh && pip install .
 python -m build
-# mv ./dist/gradio-3.32.7-py3-none-any.whl /home/fuqingxu/chatgpt_academic/docs/gradio-3.32.7-py3-none-any.whl
+
+
 mv ./dist/gradio-3.32.7-py3-none-any.whl /home/fuqingxu/faka-docker-compose/faka-docker-compose/gpt_academic/docs/gradio-3.32.7-py3-none-any.whl
 
 
